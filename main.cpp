e124acd17efab9fe65901b11008ad36c77fd445c
MEDdoger99 <Belka.sama1515@mail.ru>
2019-03-31 18:19:39 +0400

    Добавил ввывод суммы 2 чисел

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-	int x, y;
+	int x, y, ;
 	printf("ВВедите число\n");
 	scanf("%d", &x);
 	printf("Введите второе число\n");
 	scanf("%d", &y);
-}
+printf("%d", x+y);
 return 0;
+}
