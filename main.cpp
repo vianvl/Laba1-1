cf9447cafef195773fbe7c82669b36168bb0defb
MEDdoger99 <Belka.sama1515@mail.ru>
2019-03-31 18:14:58 +0400

    Добавил ввод 2 чисел

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
-#include <sdt.h>
+#include <stdio.h>
 int main(void)
 {
-
-
-
+	int x, y;
+	printf("ВВедите число\n");
+	scanf("%d", &x);
+	printf("Введите второе число\n");
+	scanf("%d", &y);
 }
 return 0;
